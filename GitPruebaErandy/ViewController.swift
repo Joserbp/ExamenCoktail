6a127cb6dbac7fdf6554129e3fec8412acc9d252
Joserbp <josera.jack@gmail.com>
2023-06-14 11:36:33 -0600

    Initial Commit

---
+++ GitPruebaErandy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPruebaErandy
+//
+//  Created by MacbookMBA7 on 14/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
