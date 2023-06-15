6af86835cdb691f4ec833bdfac42a49918dea4b6
Joserbp <josera.jack@gmail.com>
2023-06-15 17:52:26 -0600

    Add Files

---
+++ GitPruebaErandy/Model/Drink.swift
@@ -0,0 +1,8 @@
+//
+//  Drink.swift
+//  GitPruebaErandy
+//
+//  Created by MacbookMBA7 on 15/06/23.
+//
+
+import Foundation
---
+++ GitPruebaErandy/ViewModel/DrinkViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DrinkViewModel.swift
+//  GitPruebaErandy
+//
+//  Created by MacbookMBA7 on 15/06/23.
+//
+
+import Foundation
